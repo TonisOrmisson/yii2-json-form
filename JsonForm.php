8d770238ff5a367d9ac312eef0fd8009f03ed634
Tõnis Ormisson <tonis@andmemasin.eu>
2017-01-05 17:54:29 +0200

    initial

---
+++ JsonForm.php
@@ -0,0 +1,15 @@
+<?php
+namespace andmemasin\jsonform;
+
+use yii;
+use yii\base\Widget;
+/**
+ * Created by PhpStorm.
+ * User: tonis_o
+ * Date: 5.01.17
+ * Time: 17:53
+ */
+class JsonForm extends Widget
+{
+
+}
