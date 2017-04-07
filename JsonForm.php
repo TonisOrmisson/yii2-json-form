e392b7bf686964157cb8bbd0340be12280273462
Tõnis Ormisson <tonis@andmemasin.eu>
2017-04-07 19:22:48 +0300

    Add option for dynamic json (!isKeyed)

--- JsonForm.php
+++ JsonForm.php
@@ -11,11 +11,40 @@ class JsonForm extends Widget
     public $json;
     public $jsonFieldId;
     public $id = 'form-json-fields';
+    /** @var string $fieldName if no value names are specified we'll use this as default */
+    public $fieldName = 'jsonForm-values';
+    /** @var  boolean $isKeyed Whether the input fields are keyed or not. If not keyed, we can use dynamic adding */
+    public $isKeyed;
 
+    /** @var array|string input variables  */
     public $variables = [];
+
+    /** @var array input values  */
+    public $values = [];
+    /** @var  boolean $labels whether we show labels or not*/
+    public $labels = true;
+
+
+
 
     public function run()
     {
+        //if single string, then use name as key as well as label
+        if(!is_array($this->variables)){
+            $this->isKeyed = false;
+            $this->variables = [$this->variables=>[
+                'label'=>$this->variables,
+            ]];
+            $values = yii\helpers\Json::decode($this->json);
+            if($values){
+                $this->values = $values;
+            }else{
+                $this->values = $this->variables;
+            }
+        }else {
+            $this->isKeyed = true;
+            $this->values = $this->variables;
+        }
         return $this->render('json-form', [
             'widget' => $this
         ]);
--- views/json-form.php
+++ views/json-form.php
@@ -7,26 +7,73 @@ use kartik\password\PasswordInput;
 
 $currentData = \yii\helpers\Json::decode($widget->json);
 $ids = json_encode(array_keys($widget->variables));
-
+$isKeyed = json_encode($widget->isKeyed);
+$fieldName = array_keys($widget->variables)[0];
 $this->registerJs(<<<JS
- 
+
+
  var jsonFieldId = '$widget->jsonFieldId';
  var optionsArray = $ids;
  var id = '$widget->id';
- 
+ var variableKey ='$fieldName'; 
+ var btnid = '$widget->id-add';
+ var valueName = '$widget->fieldName';
+
+    var max_fields      = 10; //maximum input boxes allowed
+    var wrapper         = $("#$widget->id .container");
+   
+    var x = 0; //initial text box count
+    
+    $(wrapper).on("click",".$widget->id-add", function(e){
+        console.log('add');
+        if(x < max_fields){ //max input box allowed
+            x++; //text box increment
+            var clone = $( ".json-form-row" ).last().clone();
+            clone.find('input').val('');
+            clone.appendTo(wrapper);
+        }
+    });
+    
+    $(wrapper).on("click",".remove_field", function(e){ //user click on remove text
+        e.preventDefault(); $(this).closest('.row').remove(); x--;
+        setOptionsValues();
+    });
+    
+    $(wrapper).on("change",".values", function(e){
+         setOptionsValues();
+    });
+    
  function setOptionsValues() {
     var results = {};
-    for (i = 0; i < optionsArray.length; i++) {
-        var optionId = optionsArray[i];
-        var optIonValue = $("#"+optionId).val();
-        results[optionId] = optIonValue;
-        $("#"+jsonFieldId).val(JSON.stringify(results));
-        console.log('setting:'+optionId+'='+optIonValue);
-    }   
+    var isKeyed = $isKeyed;
+    if(isKeyed){
+        for (i = 0; i < optionsArray.length; i++) {
+            var optionId = optionsArray[i];
+            var optIonValue = $("#"+optionId).val();
+            results[optionId] = optIonValue;
+            $("#"+jsonFieldId).val(JSON.stringify(results));
+            console.log('isK'+isKeyed.toString());
+        }   
+    } else{
+        results= [];
+        $('input.values').each(function(){
+            var input = $(this);
+            results.push(input.val());
+            $("#"+jsonFieldId).val(JSON.stringify(results));
+            console.log(input.val());
+            
+        });
+        
+    }
  }
+ 
  $("#"+id+' input').change(function() {
      setOptionsValues();
  });
+ 
+ $("#"+btnid).click(function() {
+ });
+ 
 JS
     , View::POS_READY);
 
@@ -34,8 +81,10 @@ JS
 ?>
 <div id="<?=$widget->id;?>">
 <?php if(!empty($widget->variables)):?>
-    <?php foreach ($widget->variables as  $id=> $variable):?>
-        <div class="form-group field-survey-name required">
+    <div class="container">
+    <?php foreach ($widget->values as  $id=> $variable):?>
+        <div class="row json-form-row">
+
             <?php
 
                 if(isset($variable['label'])){
@@ -43,8 +92,13 @@ JS
                 }else{
                     $label = $id;
                 }
+                if(!$widget->isKeyed){
+                    $label = array_values($widget->variables)[0]['label'].' '.(intval($id)+1);
+                }
+
                 $options['id'] = $id;
-                $options['class'] = 'form-control';
+                //$options['name'] = $widget->fieldName;
+
                 if(isset($variable['options'])){
                     $options = array_merge($options,$variable['options']);
                 }
@@ -59,21 +113,35 @@ JS
                 }else{
                     $type = null;
                 }
+                if(!$widget->isKeyed){
+                    $id = $id."[0]";
+                }
+                $options['class'] = "form-control values";
             ?>
-            <label class="control-label" for="<?=Html::encode($id)?>"><?=Html::encode($label)?></label>
-            <?php if($type == 'password'):?>
+            <div class="form-group field-survey-name required col-md-<?= ($widget->isKeyed ? '12':'8')?>">
+            <?php if($widget->labels):?>
+                <label class="control-label" for="<?=Html::encode($id)?>"><?=Html::encode($label)?></label>
+            <?php endif;?>
+        <?php if($type == 'password'):?>
             <?=PasswordInput::widget([
                 'id' => $id,
                 'name' => $id,
                 'value'=>$value,
             ]);?>
             <?php else:?>
-                <?= Html::input('text',Html::encode($id),Html::encode($value),$options)?>
+                <?= \yii\bootstrap\Html::input('text',Html::encode($id),Html::encode($value),$options)?>
             <?php endif;?>
+            </div>
+            <div class="col-md-4">
+                <?php if(!$widget->isKeyed):?>
+                    <span class="btn btn-primary <?=$widget->id;?>-add" >add</span> <span class="btn btn-primary remove_field" id="<?=$widget->id;?>-remove">remove</span>
+                <?php endif;?>
+            </div>
 
         </div>
 
     <?php endforeach;?>
+    </div>
 <?php endif;?>
 
 </div>
